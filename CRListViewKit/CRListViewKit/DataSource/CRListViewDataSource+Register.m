e561851498bd60c0c26b7fa7419bbed237e8293a
chuxiaolong <sun6boys@126.com>
2018-08-03 15:40:53 +0800

    commit

--- CRListViewKit/CRListViewKit/DataSource/CRListViewDataSource+Register.m
+++ CRListViewKit/CRListViewKit/DataSource/CRListViewDataSource+Register.m
@@ -6,85 +6,94 @@
 //  Copyright © 2018年 chuxiaolong. All rights reserved.
 //
 
-#import "CRListViewDataSource+Register.h"
-#import "UITableView+Register.h"
-#import "UICollectionView+Register.h"
+#import "CRListViewDataSource.h"
 
-@implementation CRListViewDataSource (Register)
+@implementation CRListViewDataSource (TableViewRegister)
 
-//  ************************ TableView ***********************
 
 - (void)registerCellClass:(Class )cellClass forTableView:(UITableView *)tableView
 {
-    [tableView registerCellClass:cellClass];
+    [tableView registerClass:cellClass forCellReuseIdentifier:[cellClass description]];
 }
 
 - (void)registerCellNibWithClass:(Class)cellClass forTableView:(UITableView *)tableView
 {
-    
+    [tableView registerNib:[UINib nibWithNibName:NSStringFromClass(cellClass) bundle:nil] forCellReuseIdentifier:[cellClass description]];
 }
 
 - (void)registerHeaderFooterViewClass:(Class)viewClass forTableView:(UITableView *)tableView
 {
-    
+     [tableView registerClass:viewClass forCellReuseIdentifier:[viewClass description]];
 }
 
 - (void)registerHeaderFooterViewNibWithClass:(Class)viewClass forTableView:(UITableView *)tableView
 {
-    
+     [tableView registerNib:[UINib nibWithNibName:NSStringFromClass(viewClass) bundle:nil] forHeaderFooterViewReuseIdentifier:[viewClass description]];
 }
 
 //batch register cell
-- (void)registCellWithClassList:(NSArray <Class>*)calssList forTableView:(UITableView *)tableView
+- (void)registCellWithClassList:(NSArray <Class>*)classList forTableView:(UITableView *)tableView
 {
-    
+    [classList enumerateObjectsUsingBlock:^(Class  _Nonnull cellClass, NSUInteger idx, BOOL * _Nonnull stop) {
+        [self registerCellClass:cellClass forTableView:tableView];
+    }];
 }
 
-- (void)registCellNibWithClassList:(NSArray <Class>*)calssList forTableView:(UITableView *)tableView
+- (void)registCellNibWithClassList:(NSArray <Class>*)classList forTableView:(UITableView *)tableView
 {
-    
+    [classList enumerateObjectsUsingBlock:^(Class  _Nonnull cellClass, NSUInteger idx, BOOL * _Nonnull stop) {
+        [self registerCellNibWithClass:cellClass forTableView:tableView];
+    }];
 }
 
+@end
 
-//  *********************** CollectionView **********************
+
+
+@implementation CRListViewDataSource (CollectionViewRegister)
+
 - (void)registerCellClass:(Class)cellClass forCollectionView:(UICollectionView *)collectionView
 {
-    
+    [collectionView registerClass:cellClass forCellWithReuseIdentifier:[cellClass description]];
 }
 
 - (void)registerCellNibWithClass:(Class)cellClass forCollectionView:(UICollectionView *)collectionView
 {
-    
+    [collectionView registerNib:[UINib nibWithNibName:NSStringFromClass(cellClass) bundle:nil] forCellWithReuseIdentifier:[cellClass description]];
 }
 
 - (void)registerElementKindSectionHeaderViewClass:(Class)viewClass forCollectionView:(UICollectionView *)collectionView
 {
-    
+    [collectionView registerClass:viewClass forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:[viewClass description]];
 }
 
 - (void)registerElementKindSectionFooterViewClass:(Class)viewClass forCollectionView:(UICollectionView *)collectionView
 {
-    
+    [collectionView registerClass:viewClass forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:[viewClass description]];
 }
 
 - (void)registerElementKindSectionHeaderViewNibWithClass:(Class)viewClass forCollectionView:(UICollectionView *)collectionView
 {
-    
+    [collectionView registerNib:[UINib nibWithNibName:NSStringFromClass(viewClass) bundle:nil] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:[viewClass description]];
 }
 
 - (void)registerElementKindSectionFooterViewNibWithClass:(Class)viewClass forCollectionView:(UICollectionView *)collectionView
 {
-    
+    [collectionView registerNib:[UINib nibWithNibName:NSStringFromClass(viewClass) bundle:nil] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:[viewClass description]];
 }
 
 //batch register cell
-- (void)registCellWithClassList:(NSArray <Class>*)calssList forCollectionView:(UICollectionView *)collectionView
+- (void)registCellWithClassList:(NSArray <Class>*)classList forCollectionView:(UICollectionView *)collectionView
 {
-    
+    [classList enumerateObjectsUsingBlock:^(Class  _Nonnull cellClass, NSUInteger idx, BOOL * _Nonnull stop) {
+        [self registerCellClass:cellClass forCollectionView:collectionView];
+    }];
 }
 
-- (void)registCellNibWithClassList:(NSArray <Class>*)calssList forCollectionView:(UICollectionView *)collectionView
+- (void)registCellNibWithClassList:(NSArray <Class>*)classList forCollectionView:(UICollectionView *)collectionView
 {
-    
+    [classList enumerateObjectsUsingBlock:^(Class  _Nonnull cellClass, NSUInteger idx, BOOL * _Nonnull stop) {
+        [self registerCellNibWithClass:cellClass forCollectionView:collectionView];
+    }];
 }
 @end
--- CRListViewKit/CRListViewKit/DataSource/CRListViewDataSource.h
+++ CRListViewKit/CRListViewKit/DataSource/CRListViewDataSource.h
@@ -7,7 +7,41 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface CRListViewDataSource : NSObject
+
+@end
+
+
+
+@interface CRListViewDataSource (TableViewRegister)
+
+- (void)registerCellClass:(Class )cellClass forTableView:(UITableView *)tableView;
+- (void)registerCellNibWithClass:(Class)cellClass forTableView:(UITableView *)tableView;
+
+- (void)registerHeaderFooterViewClass:(Class)viewClass forTableView:(UITableView *)tableView;
+- (void)registerHeaderFooterViewNibWithClass:(Class)viewClass forTableView:(UITableView *)tableView;
+
+//batch register cell
+- (void)registCellWithClassList:(NSArray <Class>*)classList forTableView:(UITableView *)tableView;
+- (void)registCellNibWithClassList:(NSArray <Class>*)classList forTableView:(UITableView *)tableView;
+
+@end
+
+
+@interface CRListViewDataSource (CollectionViewRegister)
+
+- (void)registerCellClass:(Class)cellClass forCollectionView:(UICollectionView *)collectionView;
+- (void)registerCellNibWithClass:(Class)cellClass forCollectionView:(UICollectionView *)collectionView;
+
+- (void)registerElementKindSectionHeaderViewClass:(Class)viewClass forCollectionView:(UICollectionView *)collectionView;
+- (void)registerElementKindSectionFooterViewClass:(Class)viewClass forCollectionView:(UICollectionView *)collectionView;
+- (void)registerElementKindSectionHeaderViewNibWithClass:(Class)viewClass forCollectionView:(UICollectionView *)collectionView;
+- (void)registerElementKindSectionFooterViewNibWithClass:(Class)viewClass forCollectionView:(UICollectionView *)collectionView;
+
+//batch register cell
+- (void)registCellWithClassList:(NSArray <Class>*)classList forCollectionView:(UICollectionView *)collectionView;
+- (void)registCellNibWithClassList:(NSArray <Class>*)classList forCollectionView:(UICollectionView *)collectionView;
 
 @end
